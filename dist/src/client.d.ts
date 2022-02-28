0b09d830dddb38c7795bb1f2e7841f4efbc961e0
hugososlab <hugo.a@soslab.global>
2022-03-01 09:30:44 +1100

    Changes build setup and package json

--- dist/src/client.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Client } from './types';
-export declare const connect: (url?: string) => Promise<Client>;
-declare const _default: {
-    connect: (url?: string) => Promise<Client>;
-};
-export default _default;
--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const client_1 = (0, tslib_1.__importDefault)(require("./client"));
-exports.default = client_1.default;
-(0, tslib_1.__exportStar)(require("./types"), exports);
-//# sourceMappingURL=index.js.map
